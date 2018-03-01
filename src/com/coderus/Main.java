f2a07f23984d5148b602cbfb213aec890ca4a830
hyster <hystah@gmail.com>
2018-03-01 19:58:17 +0200

    Added fileio stuffs

--- src/com/coderus/Main.java
+++ src/com/coderus/Main.java
@@ -1,9 +1,41 @@
 package com.coderus;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("Hello Worldy -joni");
+        String filename = "small";
+        FileOutputStream out = null;
+        FileInputStream in = null;
+        File file;
+
+        try {
+            file = new File(filename +".out");
+            in = new FileInputStream(filename + "in");
+            //TODO: READ THE F'ING FILE
+
+            in.close();
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+
+            //TODO: SOLVE PROBLEM
+
+
+
+
+
+            //TODO: WRITE OUTPUT TO FILE
+            out = new FileOutputStream(file);
+            out.write(" ".getBytes());
+            out.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
